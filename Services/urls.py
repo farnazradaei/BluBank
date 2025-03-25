d2f68f0772ecf17a0b59be6bdb2a075b5e4afe4e
farnaz <farnaz.radaei@gmail.com>
2025-03-25 18:54:31 +0330

    Completing urls ✨

--- Services/urls.py
+++ Services/urls.py
@@ -2,14 +2,13 @@ from django.views import path
 from .views import Fund_transfer,Financial_management,Paying_bills,Account_recharge,Box,big_box,SIMcard_charging,Invite_friends,Lawyers_Box
 
 Urlpatterns = [
-  path('',Financial_management),
-  path('', Paying_bills),
   path('transfer', Fund_transfer),
-  path('', Account_recharge),
-  path('', Box),
-  path('', big_box),
-  path('', SIMcard_charging),
-  path('', Invite_friends),
-  path('', Lawyers_Box),
-
+  path('Paying_bills', Paying_bills),
+  path('Box', Box),
+  path('Financial_management', Financial_management),
+  path('Account_recharge', Account_recharge),
+  path('big_box', big_box),
+  path('SIMcard_charging', SIMcard_charging),
+  path('Invite_friends', Invite_friends),
+  path('Lawyers_Box', Lawyers_Box),
  ]
